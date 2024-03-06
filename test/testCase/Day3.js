ab6a68c44ef99a799c1a36d30b5f75db25eccd38
thai hien <phanthaihien000@gmail.com>
2024-03-06 15:57:26 +0700

    update 06/03/2024

--- test/testCase/Day3.js
+++ test/testCase/Day3.js
@@ -5,6 +5,10 @@ async function day3Goal(driver) {
   const elementInput = getXpath("案件名", "input");
   const value = "dev05";
   const tabName = getXpath("依頼先情報", "tab");
+  const icon = getXpath("作業依頼先部署長名", "icon");
+  const button = getXpath("検索", "button");
+  const inputPopup = getXpath("メール", "inputPopup");
+  const selectValue = By.xpath("//*[@id='1']/td[5]");
 
   const action = new Action(driver);
 
@@ -14,12 +18,12 @@ async function day3Goal(driver) {
   //test search
   await action.click(tabName);
   await driver.sleep(7000);
-  await action.click(getXpath("作業依頼先部署長名", "icon"));
+  await action.click(icon);
   await driver.sleep(3000);
-  await action.input(getXpath("メール", "inputPopup"), "dev");
-  await action.click(getXpath("検索", "button"));
+  await action.input(inputPopup, "dev");
+  await action.click(button);
   await driver.sleep(3000);
-  await action.click(By.xpath("//*[@id='1']/td[5]"));
+  await action.click(selectValue);
 
   //screenShot
   await action.takeScreenshot();
--- test/testCase/Day5.js
+++ test/testCase/Day5.js
@@ -6,6 +6,12 @@ async function day5Goal(driver) {
   const title_apl002 = "S&S作業依頼 作成";
   const moveToURL = new MoveToURL(driver);
   const action = new Action(driver);
+  const selectValue1 = By.xpath(
+    "//table[@id='database-ds-list']//tr[@id='1']//td[2]"
+  );
+  const selectValue2 = By.xpath("//tr[@id='1']//td[4]");
+  const selectTable = By.xpath("//*[contains(text(),'追加')]");
+  const icon = By.name("kyodo_hensyu_user_icon");
 
   await moveToURL.waitForTitle(title_apl002, 30000);
   await action.click(getXpath("共同編集者", "tab"));
@@ -17,14 +23,14 @@ async function day5Goal(driver) {
   );
 
   if (tableRows.length === 1) {
-    await action.click(By.xpath("//*[contains(text(),'追加')]"));
+    await action.click(selectTable);
   } else {
   }
   await driver.sleep(300);
 
   //click icon
-  await action.click(By.xpath("//tr[@id='1']//td[4]"));
-  await action.click(By.name("kyodo_hensyu_user_icon"));
+  await action.click(selectValue2);
+  await action.click(icon);
 
   //select value
   await driver.sleep(3000);
@@ -32,21 +38,13 @@ async function day5Goal(driver) {
   await driver.sleep(300);
   await action.click(getXpath("絞込み", "button"));
   await driver.sleep(300);
-  await action.click(
-    By.xpath("//table[@id='database-ds-list']//tr[@id='1']//td[2]")
-  );
+  await action.click(selectValue1);
 
   //check value select
-  const value1 = await await driver
-    .findElement(
-      By.xpath("//table[@id='database-ds-list']//tr[@id='1']//td[2]")
-    )
-    .getText();
+  const value1 = await await driver.findElement(selectValue1).getText();
   await action.click(getXpath("決定", "button"));
 
-  const value2 = await await driver
-    .findElement(By.xpath("//tr[@id='1']//td[4]"))
-    .getText();
+  const value2 = await await driver.findElement(selectValue2).getText();
 
   if (value1 === value2) {
     console.log("Match");
